eed534ef67ba96f156ca8e6801d8c81513a96847
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2025-10-24 21:55:05 +0800

    sync: page-templates/page-generated.php

--- page-templates/page-generated.php
+++ page-templates/page-generated.php
@@ -11,7 +11,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?> class="bg-black">
 
 <?php
 // Component order discovered from source
